b2fdfb254786a767fda31834241ae8ab286c0345
coder-mahfuz <mahfuz468865@gmail.com>
2022-08-02 04:39:19 +0600

    refactored code

---
+++ src/components/ChapterContainer/ChapterContainer.svelte
@@ -0,0 +1,23 @@
+<div class="ch_juz_list_chapter_container">
+  <a href="/1" class="Link_base">
+    <div class="SurahPreviewRow_container">
+      <div class="SurahPreviewRow_left">
+        <div class="SurahPreviewRow_surahNumber">
+          <span>1</span>
+        </div>
+        <div class="SurahPreviewRow_surahNameContainer">
+          <div class="SurahPreviewRow_surahName">Al-Fatihah</div>
+          <div class="SurahPreviewRow_translatedSurahName">The Opener</div>
+        </div>
+      </div>
+      <div class="SurahPreviewRow_right">
+        <div>
+          <span class="ChapterIconContainer_iconContainer">
+            <span translate="no">001</span>
+          </span>
+        </div>
+        <div class="SurahPreviewRow_description">7 Ayahs</div>
+      </div>
+    </div>
+  </a>
+</div>
